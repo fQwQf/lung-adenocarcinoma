0f0cfa31003aa3b526cb149d9c6fefec3ca3347d
fQwQf <supertjz123@foxmail.com>
2024-10-30 10:55:37 +0800

    fix: 略微优化数据

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ numpy.random.seed(seed)
 dataset = numpy.loadtxt("FeaturesWithLabels-1.csv", delimiter=",")
 
 
-#
-X = dataset[:,0:8]
-Y = dataset[:,8]
+#将数据分为输入和标签
+X = dataset[:,1:]
+Y = dataset[:,0:1]
 
-# create model
+#创建模型
 model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu')) model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(223, input_dim=8, init='uniform', activation='relu')) 
+model.add(Dense(100, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
-# Compile model
+
+#训练模型
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) # Fit the model
 model.fit(X, Y, nb_epoch=150, batch_size=10)
-# evaluate the model
+
+#评估模型
 scores = model.evaluate(X, Y)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
